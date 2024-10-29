8abf2999a394521a2450a6f789dd38701344fd55
mathieu stamm <mathieu.stamm@gmail.com>
2024-10-29 09:05:09 +0100

    indentation + changement du design avec material design

--- game-of-life.js
+++ game-of-life.js
@@ -17,8 +17,8 @@ const lines = 10;
 
 /* Préparation d'un pseudo-constructeur de cellules :*/
 function add_cell(new_x, new_y) {
-
-    let my_cell = { /* ou let my_cell = new Object();*/
+    let my_cell = {
+        /* ou let my_cell = new Object();*/
 
         /* Propriétés de la cellule :*/
         alive: false,
@@ -28,7 +28,8 @@ function add_cell(new_x, new_y) {
         /* Méthodes de la cellule :*/
         /* (Note : dans les déclarations de méthodes d'un objet, le mot-clé "this" fera référence à cet objet)*/
 
-        draw: function () { /* Sera appelée une seule fois par cellule : lorsqu'il faudra la matérialiser dans le DOM*/
+        draw: function () {
+            /* Sera appelée une seule fois par cellule : lorsqu'il faudra la matérialiser dans le DOM*/
 
             this.HTML_cell = document.createElement("div");
             /* À partir de là une nouvelle propriété est ajoutée à la cellule...*/
@@ -42,10 +43,10 @@ function add_cell(new_x, new_y) {
             this.HTML_cell.style.top = (this.y * cell_size) + "px";
 
             this.HTML_cell.addEventListener("click", this.toggle_state);
-
         },
 
-        update_HTML: function () { /* Sera appelée à chaque fois qu'il faudra changer l'état visuel de la cellule ! \ Pourrait faire réellement dépendre la présence de la classe "alive" de la valeur de la propriété alive*/
+        update_HTML: function () {
+            /* Sera appelée à chaque fois qu'il faudra changer l'état visuel de la cellule ! \ Pourrait faire réellement dépendre la présence de la classe "alive" de la valeur de la propriété alive*/
 
             this.HTML_cell.classList.toggle("alive");
         },
@@ -53,35 +54,28 @@ function add_cell(new_x, new_y) {
         toggle_state: function () {
             /* ! \ À améliorer pour ne pas être obligé de viser my_cell explicitement*/
 
-            my_cell.alive = !my_cell.alive; /* inversion de la valeur (booléenne) de la propriété alive*/
+            my_cell.alive = !my_cell
+                .alive; /* inversion de la valeur (booléenne) de la propriété alive*/
 
             my_cell.update_HTML();
-
-        }
-
-
-    }
+        },
+    };
 
     // Une cellule devra apparaître dans le DOM dès sa création :
     my_cell.draw();
 
     // Il pourrait s'avérer pratique à terme que notre pseudo-constructeur renvoie la cellule qu'il aura généré :
     return my_cell;
-
 }
 
 ///////// Génération de l'ensemble des cellules
 
 // Générer toutes les lignes...
 for (let current_y = 0; current_y < lines; current_y++) {
-
     // ...au sein desquelles il faut générer les cellules
     for (let current_x = 0; current_x < columns; current_x++) {
-
         add_cell(current_x, current_y);
-
     }
-
 }
 
 /* La suite :
